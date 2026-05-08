f8911f6dba712ae953272b3878fbc2b8d1f0535d
Lukas Kranabetter <l.kranabetter@netconomy.net>
2026-05-08 19:55:01 +0200

    feat(LUKOGEX-#6): Store progress

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Initialize Spacevim runtimepath
 
 function absolute_path(path)
   return vim.fn.fnamemodify(vim.fn.resolve(path), ':p')
@@ -8,57 +9,81 @@ function vim_arguments()
 
   for k, v in pairs(vim.v.argv) do
     if k == 1 then
-      args["nvim_bin"] = absolute_path(vim.v.argv[k])
+      args['nvim_bin'] = absolute_path(vim.v.argv[k])
 
       -- TODO: We should check here if its an asdf installation and set the paths accordingly!
-      args["nvim_lib"] = "/home/lkranabetter/.asdf/installs/neovim/0.10.4/lib/nvim"
-      args["nvim_runtime"] = "/home/lkranabetter/.asdf/installs/neovim/0.10.4/share/nvim/runtime"
+      args['nvim_lib'] = '/home/lkranabetter/.asdf/installs/neovim/0.10.4/lib/nvim'
+      args['nvim_runtime'] = '/home/lkranabetter/.asdf/installs/neovim/0.10.4/share/nvim/runtime'
     end
-    if v == "-u" then
-      args["vimrc"] = absolute_path(vim.v.argv[k + 1])
+    if v == '-u' then
+      args['vimrc'] = absolute_path(vim.v.argv[k + 1])
     end
   end
 
-  args["root_dir"] = vim.fn.fnamemodify(args["vimrc"], ':h')
+  args['root_dir'] = vim.fn.fnamemodify(args['vimrc'], ':h')
 
   return args
 end
 
 local args = vim_arguments()
 
-print(vim.inspect(vim.v.argv))
-print(vim.inspect(args))
-
-vim.env.MYVIMRC = args["vimrc"]
+-- Set related environment variables to be sure its taken everywhere consistently.
+vim.env.MYVIMRC = args['vimrc']
 -- VIMRUNTIME is used by lazy setup when rewriting rtp and needs to point to the asdf installation.
-vim.env.VIMRUNTIME = args["nvim_runtime"]
-
-print(vim.env.MYVIMRC)
-print(vim.env.VIMRUNTIME)
+vim.env.VIMRUNTIME = args['nvim_runtime']
 
 -- Reset runtimepath with asdf installation configs.
 vim.opt.rtp = {}
-vim.opt.rtp:append(args["nvim_lib"])
-vim.opt.rtp:append(args["nvim_runtime"])
-vim.opt.rtp:append(args["root_dir"])
+vim.opt.rtp:append(args['nvim_lib'])
+vim.opt.rtp:append(args['nvim_runtime'])
+vim.opt.rtp:append(args['root_dir'])
+
+-- Import needed things from runtimepath. 
+local logger = require('spacevim.logger')
+
+logger.info('Spacevim started with arguments: ' .. vim.inspect(vim.v.argv))
+logger.info('Parsed arguments: ' .. vim.inspect(args))
+logger.info('Initial runtimepath: ' .. vim.inspect(vim.opt.rtp:get()))
+
+-- Set global variables
 
 -- Spacevim root directory path needs trailing slash for further concatinations.
-vim.g._spacevim_root_dir = args["root_dir"] .. "/"
+vim.g._spacevim_root_dir = args['root_dir'] .. '/'
 
-print("Spacevim initialization.")
+-- Read needed environment variables
+local python_host_prog = os.getenv('PYTHON_HOST_PROG')
+if python_host_prog and python_host_prog ~= '' then
+  logger.info('$PYTHON_HOST_PROG is not empty, setting g:python_host_prog:' .. python_host_prog)
+  vim.g.python_host_prog = python_host_prog
+end
+
+local python3_host_prog = os.getenv('PYTHON3_HOST_PROG')
+if python3_host_prog and python3_host_prog ~= '' then
+  logger.info('$PYTHON3_HOST_PROG is not empty, setting g:python3_host_prog:' .. python3_host_prog)
+  vim.g.python3_host_prog = python3_host_prog
+end
+
+-- Spacevim initialization
+
+logger.info('Spacevim initialization.')
 vim.cmd 'call spacevim#begin()'
 vim.cmd 'call spacevim#custom#load()'
 
-local logger = require('spacevim.logger')
-
-print("Lazy configuration.")
-require("config.lazy")
+logger.info('Lazy configuration.')
+require('config.lazy')
 
 -- Why is plugin manager install not working? Is working now, just keep for history.
--- vim.opt.rtp:append("/home/lkranabetter/workspace/lukogex/spacevim/bundle/dein.vim")
+-- vim.opt.rtp:append('/home/lkranabetter/workspace/lukogex/spacevim/bundle/dein.vim')
 
-print("Runtimepath before Spacevim plugins.")
-print(vim.inspect(vim.opt.rtp:get()))
+logger.info('Runtimepath before Spacevim loads layers and plugins: ' .. vim.inspect(vim.opt.rtp:get()))
+
+if vim.fn.has('timers') then
+  vim.cmd(string.format('call timer_start(%d, "spacevim#default#keyBindings")', vim.g.spacevim_lazy_conf_timeout))
+else
+  vim.cmd 'call spacevim#default#keyBindings()'
+end
 
 -- TODO: On the long run spacevim#end plugin isntallation should be done by lazy.
 vim.cmd 'call spacevim#end()'
+
+logger.info('Spacevim finished loading.')
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -84,6 +84,10 @@ Lazy.setup({
         -- 'ibhagwan/fzf-lua',
         -- 'nvim_mini/mini.nvim',
       }
+    },
+    {
+      "saghen/blink.cmp",
+      opts = {},
     }
   },
   -- Configure any other settings here. See the documentation for more details.
